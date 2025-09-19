bba97a83a05bd2f14a7cf02a51a97ca88404b165
Anatolii <84447269+Tolik2103@users.noreply.github.com>
2025-09-19 15:37:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 from telegram.ext import Updater, MessageHandler, Filters
 
-TELEGRAM_TOKEN = '8327688810:AAEcuzr4ailCGtBThC58CEwZWWJc7Th2Ct8'
+TELEGRAM_TOKEN = 8327688810:AAEcuzr4ailCGtBThC58CEwZWWJc7Th2Ct8
 
-def handle_message(update, context):
-    message = update.message
-    if message and message.text:
-        print(f"Нове повідомлення: {message.text}")
+def handle_channel_post(update, context):
+    post = update.channel_post
+    if post and post.text:
+        print(f"Пост у каналі: {post.text}")
 
 updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+# Обробник для постів у каналі
+dispatcher.add_handler(MessageHandler(Filters.update.channel_posts, handle_channel_post))
 
-print("Бот запущено. Очікування повідомлень у групі...")
+print("Бот запущено. Очікування постів у каналі...")
 updater.start_polling()
 updater.idle()
